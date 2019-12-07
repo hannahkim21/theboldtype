0d48b11f8c65ff8f42d2d332f9726c8b7e4180ff
Hannah Kim <hannahkim@calvisitor-10-105-164-63.calvisitor.1918.berkeley.edu>
2019-12-06 16:55:46 -0800

    Work on solver

--- solver.py
+++ solver.py
@@ -4,6 +4,7 @@ sys.path.append('..')
 sys.path.append('../..')
 import argparse
 import utils
+import networkx as nx
 
 from student_utils import *
 """
@@ -25,7 +26,124 @@ def solve(list_of_locations, list_of_homes, starting_car_location, adjacency_mat
         A dictionary mapping drop-off location to a list of homes of TAs that got off at that particular location
         NOTE: both outputs should be in terms of indices not the names of the locations themselves
     """
-    pass
+    #0.0 Convert the names into indices
+    start_ind = list_of_locations.index(starting_car_location)
+    index_locs = convert_locations_to_indices(list_of_locations, list_of_locations)
+    index_locs.remove(start_ind)
+    index_homes = convert_locations_to_indices(list_of_homes, list_of_locations)
+    index_homes.remove(start_ind)
+
+    #0.1 Create the graph object
+    G = adjacency_matrix_to_graph(adjacency_matrix)[0]
+
+    #1 Degree list of tuples
+    degrees = []
+    for i in range(len(list_of_locations)):
+        tup = (i, G.degree(i))
+        degrees.append(tup)
+
+    #2 Sort by degree
+    sorted_degree = sorted(degrees, key = lambda x: -x[1])
+
+    #Cluster representation
+    cluster_dict = {}
+
+    #3 Iterate through cluster_count
+    home_count = len(index_homes)
+    for i in range(home_count):
+        # Find the node of maximal degree
+        max_node = sorted_degree[0][0]
+        cluster_dict[max_node] = []
+
+        # Determine the cutoff distance to be added
+        neighbor_list = [n for n in G.neighbors(max_node)]
+        distance_total_sum = 0
+        for n in neighbor_list:
+            distance_total_sum += nx.shortest_path_length(G, max_node, n, weight=None)
+
+        cutoff = distance_total_sum / len(neighbor_list)
+
+        # Add home nodes within cutoff distance to max degree's cluster
+        for node in neighbor_list:
+            if node in index_homes:
+                if nx.shortest_path_length(G, max_node, node, weight=None) <= cutoff:
+                    cluster_dict[max_node].append([node])
+                    # Remove nodes added from index_homes, index_locs
+                    index_homes.remove(node)
+                    index_locs.remove(node)
+
+        # Remove node of maximal degree from index_locs
+        index_locs.remove(max_node)
+        sorted_degree.pop(0)
+
+        #Check cutoff
+        k = 0
+        for h in cluster_dict.values():
+            k += len(h)
+            if k / len(list_of_homes) > .7:
+                break
+        else:
+            continue
+
+        break
+
+    # Add leftover nodes to existing clusters
+    if (len(index_homes) != 0):
+        minlen = 2500000000
+        closest = None
+        list_of_cluster = cluster_dict.keys()
+        for node in index_homes:
+            for clust in list_of_cluster:
+                if nx.shortest_path_length(G, node, clust, weight=None) < minlen:
+                    minlen = nx.shortest_path_length(G, node, clust, weight=None)
+                    closest = clust
+            cluster_dict[closest].append([node])
+
+    #4 Find the shortest path between the clusters
+    path = []
+    path.append(start_ind)
+
+    # Path from start to first cluster
+    minlen = 2500000000
+    first = None
+    for cluster in cluster_dict.items():
+        node = cluster[0]
+        if nx.shortest_path_length(G, start_ind, node, weight=None) < minlen:
+            minlen = nx.shortest_path_length(G, start_ind, node, weight=None)
+            first = node
+    path.append(first)
+
+    # Path between clusters
+    tempcluster = cluster_dict.keys()
+    minlen = 2500000000
+    curr = first
+    while tempcluster != None:
+        for cluster in cluster_dict.items():
+            node = cluster[0]
+            if nx.shortest_path_length(G, start_ind, node, weight=None) < minlen:
+                minlen = nx.shortest_path_length(G, start_ind, node, weight=None)
+                curr = node
+        path.append(curr)
+
+    last = curr
+    # Path back to start
+    path.append(nx.shortest_path_length(G, last, start_ind, weight=None))
+
+    return path, cluster_dict
+
+    # for cluster in cluster_dict:
+    #     close = None
+    #     node = cluster[0]
+    #     cluster_neighbor = [n for n in G.neighbors(node)]
+    #     minlen = 2,500,000,000
+    #     for i in cluster_neighbor:
+    #         if shortest_path_length(G, node, i, weight=None) < minlen:
+    #             minlen = shortest_path_length(G, max_node, node, weight=None)
+    #             close = i
+    #
+    #         path.append(curr_ind)
+    #
+    #     curr_ind =
 
 """
 ======================================================================
